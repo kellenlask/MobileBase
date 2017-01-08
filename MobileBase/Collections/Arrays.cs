dad98ca91bf39439acb1e8df90c290e47ae79581
Kellen Lask <kellenlask@gmail.com>
2017-01-08 12:07:29 -0700

    PCL Collections Helpers Started laying down some extra, functional collection helpers. Namely, the F#-ish Array.Init(int, func) that creates and initializes an array without having to manually declare and iterate the array. It's super clean and I hope C# gets an official implementation eventually -- looking at the code, you'll see it's not a hard thing to implement.

---
+++ MobileBase/Collections/Arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Collections
+{
+	public static class Arrays
+	{
+		/// <summary>
+		/// 	A C# implementation of the F# Array Initialization from a function. 
+		/// 	var myArray = Init<int>(5, i => i * 2); // {0, 2, 4, 6, 8}
+		/// </summary>
+		public static T[] Init<T>(int length, Func<int, T> valueFactory)
+		{
+			var array = new T[length];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = valueFactory(i);
+			}
+
+			return array;
+		}
+	}
+}
