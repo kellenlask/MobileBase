16eaf96b9f110f2bd1c66cb371c3e2cca5e0da4e
Kellen Lask <kellenlask@gmail.com>
2017-02-28 18:15:27 -0700

    More Minor Cleanup

--- MobileBase/Collections/ListHelpers.cs
+++ MobileBase/Collections/ListHelpers.cs
@@ -8,13 +8,13 @@ namespace MobileBase.Collections
 	{
 		/// <summary>
 		/// 	A C# implementation of the F# Array Initialization from a function. 
-		/// 	var myArray = Init<int>(5, i => i * 2); // {0, 2, 4, 6, 8}
+		/// 	var myArray = Init&lt;int&gt;(5, i => i * 2); // {0, 2, 4, 6, 8}
 		/// </summary>
 		public static T[] Init<T>(int length, Func<int, T> valueFactory)
 		{
 			var array = new T[length];
 
-			for (int i = 0; i < array.Length; i++)
+			for (var i = 0; i < array.Length; i++)
 			{
 				array[i] = valueFactory(i);
 			}
